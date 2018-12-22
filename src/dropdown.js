223c6c10756ea7b5fcfc4277990b642de8f84529
dlike230 <dlike230@gmail.com>
2018-12-22 17:34:30 -0500

    Initial commit

---
+++ src/dropdown.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+
+class OutClickable extends Component{
+    // a component that you can "click out" of
+    //requires that ref={this.setWrapperRef} is added as an attribute
+    constructor(props){
+        super(props);
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
+        document.addEventListener("click", this.handleClickOutside);
+    }
+
+    /**
+     * Alert if clicked on outside of element
+     */
+    handleClickOutside(event) {
+        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+            this.close_dropdown();
+        }
+    }
+
+    setWrapperRef(node) {
+        this.wrapperRef = node;
+    }
+
+}
+
+class Dropdown extends OutClickable{
+    constructor(props) {
+        super(props);
+        let starting_activity = -1;
+        //if props.def_active is not defined, it is assumed that the dropdown does not control
+        //state and instead initiates an action
+        if(props.def_active !== undefined){
+            starting_activity = props.def_active;
+        }
+        this.state = {active: false, activity: starting_activity, label_text: props.def_text};
+        this.activate_dropdown = this.activate_dropdown.bind(this);
+        this.activate_item = this.activate_item.bind(this);
+        this.close_dropdown = this.close_dropdown.bind(this);
+        this.toggle_dropdown = this.toggle_dropdown.bind(this);
+    }
+
+    close_dropdown() {
+        this.setState(state => ({active: false}));
+    }
+
+    toggle_dropdown(){
+        if(this.state.active){
+            this.close_dropdown();
+        }else{
+            this.activate_dropdown();
+        }
+    }
+
+    activate_dropdown() {
+        this.setState(state => ({active: true}));
+    }
+
+    activate_item(i) {
+        this.setState(state => ({activity: i}));
+        if(this.props.update_label){
+            //updates the label for the dropdown if this property is applied in JSX
+            this.setState(state=>({label_text: this.props.contents[i][0]}));
+        }
+        this.close_dropdown();
+        //if no default activity was selected, assumes that the item in the dropdown should not remain highlighted
+        //This is because if props.def_active is not defined, it is assumed that the dropdown does not control
+        //state and instead initiates an action
+        if(this.props.def_active === undefined){
+            this.setState(state => ({activity: -1}));
+        }
+    }
+
+    render() {
+        const a_list = [];
+        let self = this;
+        for (let i = 0; i < this.props.contents.length; i++) {
+            let addition = "";
+            if (this.state.activity === i) {
+                addition = " is-active";
+            }
+            const selected_contents = this.props.contents[i];
+            a_list.push(<a value={this.props.contents[i]} onClick={function () {
+                if(selected_contents.length > 1){
+                    //this means that a function for onclick is provided
+                    selected_contents[1]();
+                }
+                self.activate_item(i)
+            }} className={"dropdown-item" + addition} key={i}>{selected_contents[0]}</a>);
+        }
+        let addition = "";
+        if (this.state.active) {
+            addition = " is-active";
+        }
+        return (
+            <div id = {this.props.id} ref={this.setWrapperRef} className={"dropdown" + addition}>
+                <div className={"dropdown-trigger"} onClick={self.toggle_dropdown}>
+                    <button className={"button"} aria-haspopup={true} aria-controls={"dropdown-menu"}>
+                        <span>
+                            <span className={"selected_name"}>{this.state.label_text}</span>
+                            <span className="icon is-small">
+                                <i className="fa fa-angle-down" aria-hidden="true"/>
+                            </span>
+                        </span>
+                    </button>
+                </div>
+                <div className={"dropdown-menu"} role={"menu"}>
+                    <div className={"dropdown-content"}>
+                        {a_list}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class ToggleButton extends OutClickable{
+    //not a dropdown itself, but interacts with adjacent elements via css
+    constructor(props){
+        super(props);
+        this.props = props;
+        this.containerHTML = props.parent.innerHTML;
+        this.state = {active: false};
+        this.closeDropdown = this.closeDropdown.bind(this);
+        this.activateDropdown = this.activateDropdown.bind(this);
+
+    }
+
+    activateDropdown(){
+        this.setState(state=>({active:true}));
+    }
+
+    closeDropdown(){
+        this.setState(state=>({active:false}));
+    }
+
+    render(){
+        return <Button ref={this.setWrapperRef} className={"toggle_button "+this.state.active}>{this.props.name}</Button>;
+    }
+
+}
---
+++ src/search_components.js
@@ -0,0 +1,113 @@
+'use strict';
+
+let searchResultsPane = null;
+
+let offset = 0;
+
+class SearchResults extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {searchResults:[]};
+        searchResultsPane = this;
+    }
+
+    render(){
+        console.log("Re-rendering");
+        let items = [];
+        const $scope = angular.element(document.body).scope();
+        for(let i = 0; i < this.state.searchResults.length; i++){
+            const searchResult = this.state.searchResults[i];
+            if(!((!$scope.checkArr[0] || searchResult.courseReqs.indexOf($scope.checkArr[0]) > -1) &&
+                (!$scope.checkArr[1] || searchResult.courseReqs.indexOf($scope.checkArr[1]) > -1) &&
+                (!$scope.checkArr[2] || searchResult.courseReqs.indexOf($scope.checkArr[2]) > -1) &&
+                (!$scope.checkArr[3] || searchResult.courseReqs.indexOf($scope.checkArr[3]) > -1) &&
+                (!$scope.checkArr[4] || searchResult.courseReqs.indexOf($scope.checkArr[4]) > -1))){
+               continue;
+            }
+            const searchResultComponent = <SearchResult key={i + offset} course = {searchResult}>
+            </SearchResult>;
+            items.push(searchResultComponent);
+        }
+        offset += this.state.searchResults.length;
+        return <ul>{items}</ul>
+    }
+
+    updateSearchResults(oldResults){
+        let new_results = [];
+        oldResults.forEach(item => {new_results.push(item)});
+        let courseSort = angular.element(document.body).scope().courseSort;
+        let tempCourseSort = courseSort.replace("-","");
+        let scale = 1;
+        if(tempCourseSort !== courseSort){
+            scale = -1;
+            courseSort = tempCourseSort;
+        }
+        courseSort = courseSort.replace("revs.","");
+        if(courseSort === "idDashed") {
+            this.setState(state => ({
+                searchResults: new_results.sort((a, b) => {
+                    if (a.idDashed > b.idDashed){
+                        return 1;
+                    }else if (a.idDashed < b.idDashed){
+                        return -1;
+                    }else{
+                        return 0;
+                    }
+                })
+            }));
+            console.log(this.state.searchResults);
+        }else{
+            this.setState(state => ({
+                searchResults: new_results.sort((a, b) => {
+                    return scale * (a.revs[courseSort] - b.revs[courseSort])
+                })
+            }));
+            console.log(this.state.searchResults);
+        }
+    }
+
+}
+
+const updateSearchResults = function(){
+    const $scope = angular.element(document.body).scope();
+    let results = $scope.courses;
+    searchResultsPane.updateSearchResults(results);
+};
+
+class SearchResult extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.course = props.course;
+    }
+
+    render(){
+        const $scope = angular.element(document.body).scope();
+        return <li id={this.course.idDashed}
+                   onClick={() => {$scope.get.Sections(this.course.idDashed);}}>
+
+            <span className="PCR Qual"
+                  style={{background:"rgba(45, 160, 240, "+this.course.pcrQShade+")",color: this.course.pcrQColor}}>
+                {this.course.revs.cQT}
+            </span>
+
+            <span className = "PCR Diff"
+                  style = {{background:"rgba(231, 76, 60, "+this.course.pcrDShade+")",color: this.course.pcrDColor}} >
+                {this.course.revs.cDT}
+            </span>
+
+            <span className = "cID" >
+                {this.course.idSpaced}
+            </span>
+
+            <span className = "cTitle" >
+                {this.course.courseTitle}
+            </span>
+
+        </li>
+    }
+
+}
+
+ReactDOM.render(<SearchResults/>, document.querySelector("#searchResultsContainer"));
