53e2bb5820eff22d9984e310bd7eddb4cf8743d2
dlike230 <dlike230@gmail.com>
2019-01-27 13:13:10 -0500

    Fixing reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,19 @@
 const initialLocalStorageData = localStorage.getItem("schedules");
-const initSchedules = (typeof initialLocalStorageData === "undefined" ? [{
-    term: "2019A",
-    meetings: [],
-    colorPalette: [],
-    LocAdded: false
-}] : JSON.parse(initialLocalStorageData));
+const DEFAULT_SCHEDULE_NAME = "Schedule";
 
+export const generateDefaultSchedule = () => (
+    {
+        term: "2019A",
+        meetings: [],
+        colorPalette: [],
+        LocAdded: false
+    }
+);
 
 const initialState = {
-  sections: [],
-  sectionInfo: undefined,
-  schedules: initSchedules,
-  scheduleSelected:0
+    sections: [],
+    sectionInfo: undefined,
+    schedules: typeof initialLocalStorageData === "undefined" ? {[DEFAULT_SCHEDULE_NAME]: generateDefaultSchedule()} :
+        JSON.parse(initialLocalStorageData),
+    scheduleSelected: DEFAULT_SCHEDULE_NAME
 };
--- src/reducers/schedule.js
+++ src/reducers/schedule.js
@@ -1,11 +1,56 @@
-const getSchedule = (state = {}, scheduleId) => (
-    {
-        ...state,
-        scheduleSelected: scheduleId
-    }
-);
+import {generateDefaultSchedule} from "./index";
+import {ADD_SCHED_ITEM, CHANGE_SCHEDULE, CREATE_SCHEDULE, REMOVE_SCHED_ITEM} from "../actions";
 
-const createSchedule = (state = {}, scheduleName) => (
-  //TODO
-    undefined
-);
+const scheduleChange = (state = {}, action) => {
+    switch (action.type) {
+        case CREATE_SCHEDULE:
+            return {
+                ...state,
+                schedules: {
+                    ...
+                        state.schedules,
+                    [action.scheduleName]: generateDefaultSchedule(),
+                }
+            };
+        case CHANGE_SCHEDULE:
+            return {
+                ...state,
+                scheduleSelected: action.scheduleId
+            };
+        default:
+            return {
+                ...state,
+            }
+    }
+};
+
+const addRemoveScheduleItem = (state, action) => {
+    const {type, meeting} = action;
+    const activeSchedule = state.scheduleSelected;
+    switch (type) {
+        case ADD_SCHED_ITEM:
+            return {
+                ...state,
+                schedules: {
+                    ...state.schedules,
+                    [activeSchedule]: {
+                        ...state[activeSchedule],
+                        meetings: [...state.meetings, meeting]
+                    }
+                }
+            };
+        case REMOVE_SCHED_ITEM:
+            return {
+                ...state,
+                schedules: {
+                    ...state.schedules,
+                    [activeSchedule]: {
+                        ...state[activeSchedule],
+                        meetings: state.meetings.filter(m => m.idDashed !== meeting.idDashed)
+                    }
+                }
+            };
+        default:
+            return state
+    }
+};
