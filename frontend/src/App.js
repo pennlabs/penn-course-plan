2c4ba2e66fe4fd54942efb4590bd57bd40df92a1
dlike230 <dlike230@gmail.com>
2019-02-22 16:43:26 -0500

    Added in top header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,6 +32,28 @@ class App extends Component {
     render() {
         return (
             <Provider store = {store}>
+                <nav className="navbar is-link" role="navigation">
+                    <div className="navbar-brand">
+                        <div className="navbar-item nav-header">
+                            Penn Course Search
+                        </div>
+                    </div>
+                    <div className="navbar-menu">
+                        <div className="navbar-start">
+
+                        </div>
+                        <div className="navbar-end">
+
+                            <a className="navbar-item" href="https://airtable.com/shrf3pVP8e8HO2tO1">
+                                Feedback
+                            </a>
+                            <a className="navbar-item"
+                               style={{paddingRight:"0.5em"}}>About</a>
+                            <a className="navbar-item"
+                               style={{paddingRight:"0.5em"}}>Help</a>
+                        </div>
+                    </div>
+                </nav>
                 <SearchBar/>
                 <div className="App">
                     <div className="columns" id={"courses_and_sections"}>
