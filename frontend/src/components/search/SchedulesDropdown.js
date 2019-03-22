424935badf3592c17d804595fb21f1b7c78fdc96
Daniel Like <41085135+dlike230@users.noreply.github.com>
2019-03-22 17:53:32 -0400

    Fixed glitch with schedule selector

--- frontend/src/components/search/SchedulesDropdown.js
+++ frontend/src/components/search/SchedulesDropdown.js
@@ -5,7 +5,7 @@ export default class SchedulesDropdown extends Component {
 
     render() {
         return <Dropdown contents={this.props.scheduleNames.map(name => [name, () => this.props.changeSchedule(name)])}
-                         updateLabel={true} def_active={0} def_text={this.props.scheduleSelected}/>
+                         update_label={true} def_active={0} def_text={this.props.scheduleSelected}/>
     }
 }
 
