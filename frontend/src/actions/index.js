4fc3aa2039ebb68ee47040fdc21f56660ec2ba36
Daniel Like <41085135+dlike230@users.noreply.github.com>
2019-03-31 14:05:29 -0400

    Set up more actions required for requests to be made

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,21 +1,29 @@
-export const ADD_SCHED_ITEM = "ADD_SCHED_ITEM";
-export const REMOVE_SCHED_ITEM = "REMOVE_SCHED_ITEM";
+import fetch from 'cross-fetch'
+
 export const UPDATE_SEARCH = "UPDATE_SEARCH";
+export const UPDATE_SECTIONS = "UPDATE_SECTIONS";
+
 export const OPEN_SECTION_INFO = "OPEN_SECTION_INFO";
 export const CHANGE_SCHEDULE = "CHANGE_SCHEDULE";
 export const CREATE_SCHEDULE = "CREATE_SCHEDULE";
-export const UPDATE_SECTIONS = "UPDATE_SECTIONS";
+
+
 export const TOGGLE_SEARCH_FILTER = "TOGGLE_SEARCH_FILTER";
 export const OPEN_MODAL = "OPEN_MODAL";
 export const CLOSE_MODAL = "CLOSE_MODAL";
 export const ACTION_BUTTON_PRESSED = "ACTION_BUTTON_PRESSED";
+
+export const ADD_SCHED_ITEM = "ADD_SCHED_ITEM";
+export const REMOVE_SCHED_ITEM = "REMOVE_SCHED_ITEM";
 export const DELETE_SCHEDULE = "DELETE_SCHEDULE";
 export const RENAME_SCHEDULE = "RENAME_SCHEDULE";
 export const DUPLICATE_SCHEDULE = "DUPLICATE_SCHEDULE";
 export const CLEAR_SCHEDULE = "CLEAR_SCHEDULE";
+
 export const COURSE_SEARCH_ERROR = "COURSE_SEARCH_ERROR";
 export const COURSE_SEARCH_LOADING = "COURSE_SEARCH_LOADING";
 export const COURSE_SEARCH_SUCCESS = "COURSE_SEARCH_SUCCESS";
+export const REQUEST_SEARCH = "REQUEST_SEARCH";
 
 export const duplicateSchedule = scheduleName => (
     {
@@ -121,18 +129,36 @@ export const clearSchedule = () => (
     }
 );
 
-export function courseSearchError(bool) {
+export function requestSearch(searchTerm) {
+    return {
+        type: REQUEST_SEARCH,
+        searchTerm
+    }
+}
+
+export function fetchSearch(searchTerm) {
+    return (dispatch) => {
+        dispatch(requestSearch(searchTerm));
+        // placeholder URL
+        return fetch("google.com").then(
+            response => response.json(),
+            error => console.log(error)
+        ).then(json=> dispatch(updateSearch(json)));
+    }
+}
+
+export function courseSearchError() {
     return {
         type: COURSE_SEARCH_ERROR,
-        hasErrored: bool
     };
 }
-export function courseSearchLoading(bool) {
+
+export function courseSearchLoading() {
     return {
         type: COURSE_SEARCH_LOADING,
-        isLoading: bool
     };
 }
+
 export function courseSearchSuccess(items) {
     return {
         type: COURSE_SEARCH_SUCCESS,
