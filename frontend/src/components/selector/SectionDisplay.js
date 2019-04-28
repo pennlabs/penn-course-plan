40a1112eb2d8c49e99bc71ec0df79634cad8ca16
Ying Xuan <engyingxuan@gmail.com>
2019-04-28 13:16:35 -0400

    Refactored SectionDisplay.js and made whole bar clickable

--- frontend/src/components/selector/SectionDisplay.js
+++ frontend/src/components/selector/SectionDisplay.js
@@ -1,112 +1,152 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 
 export default class SectionDisplay extends Component {
-    constructor(props) {
-        super(props);
-        this.section = this.props.section;
-        this.getAddRemoveIcon = this.getAddRemoveIcon.bind(this);
-        this.getInstructorReview = this.getInstructorReview.bind(this);
-        SectionDisplay.getPcaButton = SectionDisplay.getPcaButton.bind(this);
+    stripTime = (s) => {
+        let newS = s.replace(" to ", "-");
+        newS = newS.replace("on", "");
+        return newS;
     }
 
-    getAddRemoveIcon() {
+    justSection = s => s.substring(s.lastIndexOf(" ") + 1);
+
+    getAddRemoveIcon = () => {
+        const {
+            addSchedItem,
+            removeSchedItem,
+            section: {
+                revs,
+                fullSchedInfo,
+            },
+            inSchedule,
+        } = this.props;
+
         let className = "fa";
-        const addSchedItem = this.props.addSchedItem;
-        const removeSchedItem = this.props.removeSchedItem;
-        if (!this.props.inSchedule) {
+        if (!inSchedule) {
             className += " fa-plus";
         } else {
             className += " fa-times";
         }
-        let onClick = undefined;
+        let onClick;
 
-        const section = this.section;
-        if (!this.props.inSchedule) {
+        if (!inSchedule) {
             onClick = () => {
-                addSchedItem({...section.fullSchedInfo[0], revs: section.revs});
+                addSchedItem({ ...fullSchedInfo[0], revs });
             };
         } else {
-            onClick = function () {
-                removeSchedItem(section.fullSchedInfo[0].fullID);
+            onClick = () => {
+                removeSchedItem(fullSchedInfo[0].fullID);
             };
         }
 
-        return <i className={className}
-                  onClick={onClick}/>;
+        return <i className={className} onClick={onClick} role="button" />;
     }
 
-    static getPcaButton() {
-        const onClick = function () {
-        };
-        return <i className={"far fa-bell"}
-                  onClick={onClick}
-                  title="Penn Course Alert"/>;
+    getPcaButton = () => {
+        let onClick;
+        return (
+            <i
+                className="far fa-bell"
+                onClick={onClick}
+                title="Penn Course Alert"
+                role="button"
+            />
+        );
     }
 
-    getInstructorReview() {
-        const bgColor = "rgba(46, 204, 113," + this.section.pcrIShade + ")";
-        return <span className={"PCR Inst"}
-                     style={{background: bgColor, color: this.section.pcrIColor}}
-                     onClick={this.props.openSection}>{this.section.revs.cI}</span>;
-    }
+    getInstructorReview = () => {
+        const {
+            section: {
+                revs,
+                pcrIShade,
+                pcrIColor,
+            },
+        } = this.props;
 
-    static justSection(s) {
-        return s.substring(s.lastIndexOf(' ') + 1)
-    }
-
-    static stripTime(s) {
-        s = s.replace(' to ', '-');
-        s = s.replace('on', '');
-        return s
+        const bgColor = `rgba(46, 204, 113, ${pcrIShade})`;
+        return (
+            <span
+                className="PCR Inst"
+                style={{ background: bgColor, color: pcrIColor }}
+            >
+                { revs.cI }
+            </span>
+        );
     }
 
     render() {
-        let className = this.section.actType;
-        if (this.section === this.section.idSpaced.replace(' ', '').replace(' ', '')) {
+        const {
+            section,
+            overlap,
+            openSection,
+        } = this.props;
+
+        let className = section.actType;
+        // Not quite sure why className is actType
+        if (section === section.idSpaced.replace(" ", "").replace(" ", "")) {
             className += " activeItem";
         }
-        if(this.props.overlap){
+        if (overlap) {
             className += "hideSec";
         }
-        /*if((!$scope.sched.SecOverlap(this.section) && $scope.schedSections.indexOf(this.section.idDashed) === -1)){
+        /* if((!$scope.sched.SecOverlap(this.section) 
+            && $scope.schedSections.indexOf(this.section.idDashed) === -1)){
             className += "hideSec";
-        }*/
-        return <li
-            id={this.section.idDashed}
-            className={className}>
-            <div className={"columns is-gapless"}>
+        } */
+        return (
+            <li
+                id={section.idDashed}
+                className={className}
+                onClick={openSection}
+                style={{ cursor: "pointer" }}
+                role="menuitem"
+                // could be incorporated to css
+            >
+                <div className="columns is-gapless">
+                    <div className="column is-one-fifth">
+                        { this.getAddRemoveIcon() }
+                        <span
+                            className={`statusClass ${section.isOpen ? "openSec" : "closedSec"}`}
+                        >
+                            { (!section.isOpen) && this.getPcaButton() }
+                        </span>
+                    </div>
 
-                <div className={"column is-one-fifth"}>
-                    {this.getAddRemoveIcon()}
-                    <span className={"statusClass " + (this.section.isOpen ? "openSec" : "closedSec")}
-                          onClick={this.props.openSection}>
-                      {(!this.section.isOpen) && SectionDisplay.getPcaButton()}
-                  </span>
-                </div>
+                    <div className="column is-one-fifth">
+                        { this.getInstructorReview() }
+                    </div>
 
-                <div className="column is-one-fifth">
-                    {this.getInstructorReview()}
-                </div>
+                    <div className="column is-one-fifth" style={{ marginLeft: "0.4rem" }}>
+                        <span
+                            className="sectionText"
+                        >
+                            { this.justSection(section.idSpaced) }
+                        </span>
+                    </div>
 
-                <div className={"column is-one-fifth"} style={{marginLeft: "0.4rem"}}>
-                  <span className="sectionText"
-                        onClick={this.props.openSection}>
-                      {
-                          SectionDisplay.justSection(this.section.idSpaced)
-                      }
-                  </span>
+                    <div className="column">
+                        <span
+                            className="sectionText"
+                        >
+                            { this.stripTime(section.timeInfo) }
+                        </span>
+                    </div>
                 </div>
-
-                <div className={"column"}>
-                  <span className={"sectionText"}
-                        onClick={this.props.openSection}>
-                      {
-                          SectionDisplay.stripTime(this.section.timeInfo)
-                      }
-                  </span>
-                </div>
-            </div>
-        </li>
+            </li>
+        );
     }
 }
+
+SectionDisplay.propTypes = {
+    addSchedItem: PropTypes.func.isRequired,
+    removeSchedItem: PropTypes.func.isRequired,
+    openSection: PropTypes.func.isRequired,
+    inSchedule: PropTypes.bool.isRequired,
+    overlap: PropTypes.bool.isRequired,
+    section: {
+        revs: PropTypes.number,
+        pcrIShade: PropTypes.number,
+        pcrIColor: PropTypes.number,
+    },
+};
