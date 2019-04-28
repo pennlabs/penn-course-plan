1729e240aa9a433eea50331ea0f4a11292cf00f8
Daniel Like <41085135+dlike230@users.noreply.github.com>
2019-04-28 12:51:23 -0400

    Basic searching working

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,4 +1,4 @@
-import fetch from 'cross-fetch'
+import fetch from "cross-fetch";
 
 export const UPDATE_SEARCH = "UPDATE_SEARCH";
 export const UPDATE_SECTIONS = "UPDATE_SECTIONS";
@@ -28,7 +28,7 @@ export const REQUEST_SEARCH = "REQUEST_SEARCH";
 export const duplicateSchedule = scheduleName => (
     {
         type: DUPLICATE_SCHEDULE,
-        scheduleName
+        scheduleName,
     }
 );
 
@@ -41,63 +41,63 @@ export const deleteSchedule = () => (
 export const renameSchedule = scheduleName => (
     {
         type: RENAME_SCHEDULE,
-        scheduleName
+        scheduleName,
     }
 );
 
 export const changeSchedule = scheduleId => (
     {
         type: CHANGE_SCHEDULE,
-        scheduleId
+        scheduleId,
     }
 );
 
 export const addSchedItem = courseObj => (
     {
         type: ADD_SCHED_ITEM,
-        courseObj
+        courseObj,
     }
 );
 
 export const removeSchedItem = idDashed => (
     {
         type: REMOVE_SCHED_ITEM,
-        idDashed
+        idDashed,
     }
 );
 
 export const updateSearch = searchResults => (
     {
         type: UPDATE_SEARCH,
-        searchResults
+        searchResults,
     }
 );
 
 export const updateSections = sections => (
     {
         type: UPDATE_SECTIONS,
-        sections
+        sections,
     }
 );
 
 export const updateSectionInfo = sectionInfo => (
     {
         type: OPEN_SECTION_INFO,
-        sectionInfo
+        sectionInfo,
     }
 );
 
 export const createSchedule = scheduleName => (
     {
         type: CREATE_SCHEDULE,
-        scheduleName
+        scheduleName,
     }
 );
 
 export const toggleSearchFilterShown = location => (
     {
         type: TOGGLE_SEARCH_FILTER,
-        location
+        location,
     }
 );
 
@@ -105,7 +105,7 @@ export const toggleSearchFilterShown = location => (
 export const openModal = modalShown => (
     {
         type: OPEN_MODAL,
-        modalShown
+        modalShown,
     }
 );
 
@@ -116,24 +116,24 @@ export const closeModal = () => (
     }
 );
 
-export const triggerModalAction = (modalAction) => (
+export const triggerModalAction = modalAction => (
     {
         type: ACTION_BUTTON_PRESSED,
-        modalAction
+        modalAction,
     }
 );
 
 export const clearSchedule = () => (
     {
-        type: CLEAR_SCHEDULE
+        type: CLEAR_SCHEDULE,
     }
 );
 
 export function requestSearch(searchData) {
     return {
         type: REQUEST_SEARCH,
-        searchData
-    }
+        searchData,
+    };
 }
 
 
@@ -150,32 +150,32 @@ const preprocessSearchData = (searchData) => {
 
 function buildSearchUrl(searchData) {
     searchData = preprocessSearchData(searchData);
-    let url = '/Search?searchType=' + searchData.searchType + "&resultType=" + searchData.resultType + "&searchParam=" + searchData.param;
+    const url = `/Search?searchType=${searchData.searchType}&resultType=${searchData.resultType}&searchParam=${searchData.param}`;
     console.log(url);
     return url;
 }
 
-const processSearchData = (searchData) => searchData.map(item => {
-    let qFrac = item.revs.cQ / 4;
-    let dFrac = item.revs.cD / 4;
-    let iFrac = item.revs.cI / 4;
+const processSearchData = searchData => searchData.map((item) => {
+    const qFrac = item.revs.cQ / 4;
+    const dFrac = item.revs.cD / 4;
+    const iFrac = item.revs.cI / 4;
     item.pcrQShade = Math.pow(qFrac, 3) * 2; // This is the opacity of the PCR block
     item.pcrDShade = Math.pow(dFrac, 3) * 2;
     item.pcrIShade = Math.pow(iFrac, 3) * 2;
     if (qFrac < 0.50) {
-        item.pcrQColor = 'black';
+        item.pcrQColor = "black";
     } else {
-        item.pcrQColor = 'white';
+        item.pcrQColor = "white";
     } // It's hard to see white text on a light background
     if (dFrac < 0.50) {
-        item.pcrDColor = 'black';
+        item.pcrDColor = "black";
     } else {
-        item.pcrDColor = 'white';
+        item.pcrDColor = "white";
     }
     if (iFrac < 0.50) {
-        item.pcrIColor = 'black';
+        item.pcrIColor = "black";
     } else {
-        item.pcrIColor = 'white';
+        item.pcrIColor = "white";
     }
     item.revs.QDratio = item.revs.cQ - item.revs.cD; // This is my way of calculating if a class is "good and easy." R > 1 means good and easy, < 1 means bad and hard
 
@@ -186,16 +186,24 @@ const processSearchData = (searchData) => searchData.map(item => {
     // the rating as a string - let's us make the actual rating something else and still show the correct number
     item.revs.cQT = item.revs.cQ.toFixed(2);
     if (item.revs.cQ === 0) {
-        item.revs.cQT = '';
+        item.revs.cQT = "";
     }
     item.revs.cDT = item.revs.cD.toFixed(2);
     if (item.revs.cD === 0) {
-        item.revs.cDT = '';
+        item.revs.cDT = "";
         item.revs.QDratio = -100;
         item.revs.cD = 100;
     }
     return item;
 });
+
+
+export function courseSearchError(error) {
+    return {
+        type: COURSE_SEARCH_ERROR,
+        error,
+    };
+}
 
 export function fetchSearch(searchData) {
     return (dispatch) => {
@@ -203,17 +211,10 @@ export function fetchSearch(searchData) {
         return fetch(buildSearchUrl(searchData)).then(
             response => response.json().then(
                 json => dispatch(updateSearch(processSearchData(json))),
-                error => dispatch(courseSearchError(response.status))
+                error => dispatch(courseSearchError(error)),
             ),
-            error => dispatch(courseSearchError(error))
+            error => dispatch(courseSearchError(error)),
         );
-    }
-}
-
-export function courseSearchError(error) {
-    return {
-        type: COURSE_SEARCH_ERROR,
-        error
     };
 }
 
@@ -226,6 +227,6 @@ export function courseSearchLoading() {
 export function courseSearchSuccess(items) {
     return {
         type: COURSE_SEARCH_SUCCESS,
-        items
+        items,
     };
 }
