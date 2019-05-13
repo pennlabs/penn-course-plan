e409d72e610458d156eeb4df2f18ac23afe782da
Davis Haupt <davis.haupt@gmail.com>
2019-05-13 12:10:52 -0400

    fixed linting errors

--- frontend/src/components/schedule/Block.js
+++ frontend/src/components/schedule/Block.js
@@ -1,38 +1,63 @@
-import React, {Component} from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default class BlockNew extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const days = ['M', 'T', 'W', 'R', 'F', 'S', 'U'];
-        let {offsets, meeting, course, remove, style} = this.props;
-        let {day, start, end} = meeting;
-        let {id, color, coreqFulfilled} = course;
-        const pos = {
-            gridRowStart: (start - offsets.time) * 2 + offsets.row+1,
-            gridRowEnd: (end - offsets.time) * 2 + offsets.col+1,
-            gridColumn: days.indexOf(day) + 1 + offsets.col,
-            position: 'relative',
-        }
-        return (
-            <div className={`block ${color}`} style={{...pos, ...style}}>
-                <div className={'inner-block'}>
-                    <span
-                        className={'remove'}
-                        onClick={e => {
-                            remove();
-                            e.stopPropagation();
-                        }}
-                    ><i className="fas fa-times" /></span>
-                    <span
-                        className={coreqFulfilled ? 'hide' : ''}
-                        title={"Registration is required for an associated section."}
-                    ><i className="fas fa-exclamation warning" /></span>
-                    <span>{id.replace(/-/g, ' ')}</span>
-                </div>
+export default function Block(props) {
+    const days = ["M", "T", "W", "R", "F", "S", "U"];
+    const {
+        offsets, meeting, course, remove, style,
+    } = props;
+    const { day, start, end } = meeting;
+    const { id, color, coreqFulfilled } = course;
+    const pos = {
+        gridRowStart: (start - offsets.time) * 2 + offsets.row + 1,
+        gridRowEnd: (end - offsets.time) * 2 + offsets.col + 1,
+        gridColumn: days.indexOf(day) + 1 + offsets.col,
+        position: "relative",
+    };
+    return (
+        <div className={`block ${color}`} style={{ ...pos, ...style }}>
+            <div className="inner-block">
+                <span
+                    role="button"
+                    className="remove"
+                    onClick={(e) => {
+                        remove();
+                        e.stopPropagation();
+                    }}
+                >
+                    <i className="fas fa-times" />
+                </span>
+                <span
+                    className={coreqFulfilled ? "hide" : ""}
+                    title="Registration is required for an associated section."
+                >
+                    <i className="fas fa-exclamation warning" />
+                </span>
+                <span>{id.replace(/-/g, " ")}</span>
             </div>
-        )
-    }
+        </div>
+    );
 }
+
+Block.propTypes = {
+    offsets: PropTypes.shape({
+        time: PropTypes.number,
+        row: PropTypes.number,
+        col: PropTypes.number,
+    }),
+    meeting: PropTypes.shape({
+        day: PropTypes.string,
+        start: PropTypes.number,
+        end: PropTypes.number,
+    }),
+    course: PropTypes.shape({
+        id: PropTypes.string,
+        color: PropTypes.string,
+        coreqFulfilled: PropTypes.bool,
+    }),
+    remove: PropTypes.func,
+    style: PropTypes.shape({
+        width: PropTypes.string,
+        left: PropTypes.string,
+    }),
+};
--- frontend/src/components/schedule/Schedule.js
+++ frontend/src/components/schedule/Schedule.js
@@ -1,85 +1,92 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import connect from "react-redux/es/connect/connect";
 
-import {removeSchedItem} from "../../actions";
+import { removeSchedItem } from "../../actions";
 
-import './schedule.css'
-import Days from './Days'
-import Times from './Times'
-import Block from './Block'
-import GridLines from './GridLines'
+import "./schedule.css";
+import Days from "./Days";
+import Times from "./Times";
+import Block from "./Block";
+import GridLines from "./GridLines";
 
 // Used for box coloring, from StackOverflow:
 // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
-String.prototype.hashCode = function() {
-    let hash = 0, i, chr;
-    if (this.length === 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        chr   = this.charCodeAt(i);
-        hash  = ((hash << 5) - hash) + chr;
+const hashString = (s) => {
+    let hash = 0;
+    if (s.length === 0) return hash;
+    for (let i = 0; i < s.length; i += 1) {
+        const chr = s.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
         hash |= 0; // Convert to 32bit integer
     }
     return hash;
 };
 
+
 // From an array of meetings, get the groups which conflict in timing.
 export const getConflictGroups = (meetings) => {
     // returns true if the two meetings conflict.
     const overlap = (m1, m2) => {
-        let start1 = m1.data.start;
-        let start2 = m2.data.start;
-        let end1 = m1.data.end;
-        let end2 = m2.data.end;
-        return m1.data.day === m2.data.day && !(end1 <= start2 || end2 <= start1)
-    }
+        const start1 = m1.data.start;
+        const start2 = m2.data.start;
+        const end1 = m1.data.end;
+        const end2 = m2.data.end;
+        return m1.data.day === m2.data.day && !(end1 <= start2 || end2 <= start1);
+    };
     // get a unique ID for a course's meeting
-    const id = m => {
-        return `${m.course.id}-${m.data.day}-${m.data.start}-${m.data.end}`
-    }
+    const id = m => `${m.course.id}-${m.data.day}-${m.data.start}-${m.data.end}`;
 
     // `conflicts` is a union-find datastructure representing "conflict sets".
     // https://en.wikipedia.org/wiki/Disjoint-set_data_structure
     // meetings m1 and m2 are in the same conflict set if m1 and m2 conflict
     // with at least one meeting m3 which is also in the set (m3 can be m1 or m2).
-    let conflicts = {};
+    const conflicts = {};
     const merge = (m1, m2) => {
-        conflicts[id(m1)] = conflicts[id(m2)] = new Set([...conflicts[id(m1)], ...conflicts[id(m2)]]);
-    }
+        conflicts[id(m2)] = new Set(
+            [...conflicts[id(m1)], ...conflicts[id(m2)]]
+        );
+        conflicts[id(m1)] = conflicts[id(m2)];
+    };
 
-    meetings.forEach(m => {
-        conflicts[id(m)] = new Set([m])
-    })
+    meetings.forEach((m) => {
+        conflicts[id(m)] = new Set([m]);
+    });
 
     // compare every pair of meetings. if they overlap, merge their sets.
-    for (let i = 0; i < meetings.length-1; i++) {
-        for (let j = i+1; j < meetings.length; j++) {
+    for (let i = 0; i < meetings.length - 1; i += 1) {
+        for (let j = i + 1; j < meetings.length; j += 1) {
             if (overlap(meetings[i], meetings[j])) {
-                merge(meetings[i], meetings[j])
+                merge(meetings[i], meetings[j]);
             }
         }
     }
 
     // remove sets of size 1 from the results; they're not conflicting with anything.
-    for (const key of Object.keys(conflicts)) {
+    Object.keys(conflicts).forEach((key) => {
         if (conflicts[key].size <= 1) {
-            delete conflicts[key]
+            delete conflicts[key];
         }
-    }
+    });
     // use a Set to remove duplicates, so we get only unique conflict sets.
     return Array.from(new Set(Object.values(conflicts)).values());
-}
+};
 
 class Schedule extends Component {
-    constructor(props) {
-        super(props);
+    propTypes = {
+        schedData: PropTypes.shape({
+            meetings: PropTypes.array,
+        }),
+        removeSection: PropTypes.func,
     }
 
     render() {
-        let {schedData, removeSection} = this.props;
-        let sections = schedData.meetings || [];
+        const { schedData, removeSection } = this.props;
+        const sections = schedData.meetings || [];
 
         if (sections.length < 1) {
-            return <EmptySchedule />
+            return <EmptySchedule />;
         }
 
         let startHour = 10.5;
@@ -87,92 +94,91 @@ class Schedule extends Component {
 
         // get the minimum start hour and the max end hour to set bounds on the schedule.
         startHour = Math.floor(
-            Math.min(startHour, ...sections.map(m => m.meetHour)) - .5
+            Math.min(startHour, ...sections.map(m => m.meetHour)) - 0.5
         );
         endHour = Math.ceil(
-            Math.max(endHour, ...sections.map(m => m.meetHour + m.hourLength)) + .5
+            Math.max(endHour, ...sections.map(m => m.meetHour + m.hourLength)) + 0.5
         );
 
         // show the weekend days only if there's a section which meets on saturday (S) or sunday (U)
         const showWeekend = sections.filter(
-            sec => sec.meetDay.indexOf('S') !== -1 || sec.meetDay.indexOf('U') !== -1).length > 0;
+            sec => sec.meetDay.indexOf("S") !== -1 || sec.meetDay.indexOf("U") !== -1
+        ).length > 0;
 
         // actual schedule elements are offset by the row/col offset since
         // days/times take up a row/col respectively.
-        let rowOffset = 1;
-        let colOffset = 1;
+        const rowOffset = 1;
+        const colOffset = 1;
 
-        const getNumRows = () => {
-            return (Math.ceil(endHour) - Math.floor(startHour)) * 2 + rowOffset;
-        }
-        const getNumCol = () => {
-            return 5 + colOffset + (showWeekend ? 2 : 0);
-        }
+        const getNumRows = () => (Math.ceil(endHour) - Math.floor(startHour)) * 2 + rowOffset;
+        const getNumCol = () => 5 + colOffset + (showWeekend ? 2 : 0);
 
         // step 2 in the CIS121 review: hashing with linear probing.
         // hash every section to a color, but if that color is taken, try the next color in the
         // colors array. Only start reusing colors when all the colors are used.
         const getColor = (() => {
             const colors = ["blue", "red", "aqua", "orange", "green", "pink", "sea", "indigo"];
-            let used = []; // some CIS120: `used` is a *closure* storing the colors currently in the schedule
-            return c => {
+            // some CIS120: `used` is a *closure* storing the colors currently in the schedule
+            let used = [];
+            return (c) => {
                 if (used.length === colors.length) {
                     // if we've used all the colors, it's acceptable to start reusing colors.
                     used = [];
                 }
-                let i = c.hashCode();
+                let i = hashString(c);
                 while (used.indexOf(colors[i % colors.length]) !== -1) {
-                    i++;
+                    i += 1;
                 }
-                const color = colors[i % colors.length]
-                used.push(color)
+                const color = colors[i % colors.length];
+                used.push(color);
                 return color;
-            }
-        })()
+            };
+        })();
         const sectionIds = sections.map(x => x.idDashed);
-        let meetings = []; // a meeting is the data that represents a single block on the schedule.
-        sections.forEach(m => {
-            let days = m.meetDay.split(''); // generate as many meetings as there are days
+        // a meeting is the data that represents a single block on the schedule.
+        const meetings = [];
+        sections.forEach((m) => {
+            const days = m.meetDay.split(""); // generate as many meetings as there are days
             const color = getColor(m.idDashed);
-            meetings.push(...days.map(d => {
-                return {
+            meetings.push(...days.map(d => (
+                {
                     data: {
                         day: d,
                         start: m.meetHour,
-                        end: m.meetHour + m.hourLength
+                        end: m.meetHour + m.hourLength,
                     },
                     course: {
+                        color,
                         id: m.idDashed,
                         fullID: m.fullID,
-                        color: color,
                         coreqFulfilled: m.SchedAsscSecs.filter(
                             coreq => sectionIds.indexOf(coreq) !== -1
-                        ).length > 0
+                        ).length > 0,
                     },
                     style: {
-                        width: '100%',
+                        width: "100%",
                         left: 0,
-                    }
+                    },
                 }
-            }))
-        })
-        let conflicts = getConflictGroups(meetings)
-        for (const conflict of conflicts) {
+            )));
+        });
+
+        getConflictGroups(meetings).forEach((conflict) => {
             // for every conflict of size k, make the meetings in that conflict
             // take up (100/k) % of the square, and use `left` to place them
             // next to each other.
-            const group = Array.from(conflict.values())
+            const group = Array.from(conflict.values());
             const w = 100 / group.length;
-            for (let j = 0; j < group.length; j++) {
+            for (let j = 0; j < group.length; j += 1) {
                 group[j].style = {
                     width: `${w}%`,
-                    left: `${w*j}%`
-                }
+                    left: `${w * j}%`,
+                };
             }
-        }
+        });
         // generate actual block components.
         // position in grid is determined by the block given the meeting info and grid offsets.
-        let blocks = meetings.map(meeting => (
+        const blocks = meetings.map(meeting => (
             <Block
                 meeting={meeting.data}
                 course={meeting.course}
@@ -185,15 +191,15 @@ class Schedule extends Component {
                 key={`${meeting.course.id}-${meeting.data.day}`}
                 remove={() => removeSection(meeting.course.fullID)}
             />
-        ))
+        ));
 
-        let dims = {
+        const dims = {
             gridTemplateColumns: `.4fr repeat(${getNumCol() - 1}, 1fr)`,
             gridTemplateRows: `repeat(${getNumRows()}, 1fr)`,
-        }
+        };
 
         return (
-            <div className={'schedule box'} style={dims}>
+            <div className="schedule box" style={dims}>
                 <Days offset={colOffset} weekend={showWeekend} />
                 <Times
                     startTime={startHour}
@@ -208,32 +214,35 @@ class Schedule extends Component {
                 />
                 {blocks}
             </div>
-        )
+        );
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        schedData: state.schedule.schedules[state.schedule.scheduleSelected]
-    };
-};
-
-const mapDispatchToProps = (dispatch) => (
+const mapStateToProps = state => (
     {
-        removeSection: idDashed => dispatch(removeSchedItem(idDashed))
+        schedData: state.schedule.schedules[state.schedule.scheduleSelected],
+    }
+);
+
+
+const mapDispatchToProps = dispatch => (
+    {
+        removeSection: idDashed => dispatch(removeSchedItem(idDashed)),
     }
 );
 
 export default connect(mapStateToProps, mapDispatchToProps)(Schedule);
 
 const EmptySchedule = () => (
-    <div className={'box'} style={{height: '100%'}}>
-        <p style={{fontSize: "1.5em", marginTop: "7em", display: "block"}}>
-            Search for courses above <br/>then click a section's + icon to add it to the schedule.
+    <div className="box" style={{ height: "100%" }}>
+        <p style={{ fontSize: "1.5em", marginTop: "7em", display: "block" }}>
+            Search for courses above
+            <br />
+            then click a section&#39;s + icon to add it to the schedule.
         </p>
-        <p style={{fontSize: "1em"}}>
+        <p style={{ fontSize: "1em" }}>
             These are mock schedules.
-            <br/>
+            <br />
             You still need to register for your classes on Penn InTouch.
         </p>
     </div>
