f7d52eb9feb5db1f854a1865a415a271cc9d5d48
Ying Xuan <engyingxuan@gmail.com>
2019-05-15 23:21:51 -0400

    Refactored components

--- frontend/src/components/selector/SectionList.js
+++ frontend/src/components/selector/SectionList.js
@@ -1,41 +1,62 @@
-import React, {Component} from 'react';
-import SectionDisplay from './SectionDisplay'
-import {section_info_a} from "../../sections_data";
+import React from "react";
+import PropTypes from "prop-types";
+import SectionDisplay from "./SectionDisplay";
+import { sectionInfoA } from "../../sections_data";
 
-export default class SectionList extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        let sections = [];
-        const self = this;
-        const updateSectionInfo = this.props.updateSectionInfo;
-        const psections = this.props.sections || [];
-        console.log('davis', psections.length)
-        for (let i = 0; i < psections.length; i++) {
-            let section = psections[i];
-            sections.push(<SectionDisplay
-                inSchedule={this.props.scheduleContains(section.idDashed)}
-                overlap={this.props.overlaps(section)}
-                addSchedItem={this.props.addSchedItem}
-                removeSchedItem={this.props.removeSchedItem}
-                section={section} key={i}
+export default function SectionList(props) {
+    const {
+        updateSectionInfo,
+        sections,
+        scheduleContains,
+        overlaps,
+        addSchedItem,
+        removeSchedItem,
+        listRef,
+    } = props;
+    const sectionsArr = [];
+    for (let i = 0; i < sections.length; i += 1) {
+        const section = sections[i];
+        sectionsArr.push(
+            <SectionDisplay
+                inSchedule={scheduleContains(section.idDashed)}
+                overlap={overlaps(section)}
+                addSchedItem={addSchedItem}
+                removeSchedItem={removeSchedItem}
+                section={section}
+                key={i}
                 openSection={() => {
-                    updateSectionInfo(section_info_a);
-                }}/>);
-            /*if (($scope.showAct === section.actType || $scope.showAct === 'noFilter') &&
-                (section.isOpen || $scope.showClosed) &&
-                ($scope.currentCourse || $scope.starSections.indexOf(section.idDashed) > -1)) {
-                sections.push(<SectionDisplay section={section} key={i}/>);
-            }*/
-        }
-
-        return <div id="SectionList" ref={this.props.listRef}>
-            <ul>
-                {sections}
-            </ul>
-        </div>;
+                    updateSectionInfo(sectionInfoA);
+                }}
+            />
+        );
+        /* if (($scope.showAct === section.actType || $scope.showAct === 'noFilter') &&
+            (section.isOpen || $scope.showClosed) &&
+            ($scope.currentCourse || $scope.starSections.indexOf(section.idDashed) > -1)) {
+            sectionsArr.push(<SectionDisplay section={section} key={i}/>);
+        } */
     }
 
+    return (
+        <div id="SectionList" ref={listRef}>
+            <ul>
+                {sectionsArr}
+            </ul>
+        </div>
+    );
 }
+
+SectionList.propTypes = {
+    updateSectionInfo: PropTypes.func.isRequired,
+    sections: PropTypes.arrayOf(PropTypes.object),
+    scheduleContains: PropTypes.func.isRequired,
+    overlaps: PropTypes.func.isRequired,
+    addSchedItem: PropTypes.func,
+    removeSchedItem: PropTypes.func,
+    listRef: PropTypes.shape({
+        current: PropTypes.object,
+    }),
+};
+
+SectionList.defaultProps = {
+    sections: [],
+};
