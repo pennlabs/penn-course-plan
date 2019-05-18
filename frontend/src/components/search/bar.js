1c3a00d233fb8b5744a24de491fe443ca09335eb
Ying Xuan <engyingxuan@gmail.com>
2019-05-17 20:59:01 -0400

    Refactored /search

--- frontend/src/components/search/bar.js
+++ frontend/src/components/search/bar.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import connect from "react-redux/es/connect/connect";
 import { Dropdown } from "../dropdown";
 import SummaryDropdown from "./summary";
 import {
-    changeSchedule, fetchCourseSearch, openModal, toggleSearchFilterShown,
+    changeSchedule, fetchCourseSearch, openModal, toggleSearchFilterShown
 } from "../../actions";
 import { NEW_SCHEDULE_MODAL_NAME } from "../modals/new_schedule_modal";
 import SchedulesDropdown from "./SchedulesDropdown";
@@ -16,101 +17,128 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = { searchFilterOpened: false };
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(event) {
+    handleSubmit = (event) => {
+        const {
+            startSearch,
+        } = this.props;
         event.preventDefault();
-        this.props.startSearch({ searchType: "courseIDSearch", param: event.target.getElementsByTagName("input")[0].value });
+        startSearch({ searchType: "courseIDSearch", param: event.target.getElementsByTagName("input")[0].value });
     }
 
-    searchToggler() {
-        const self = this;
-        const selectedBackground = this.state.searchFilterOpened ? "images/filter_b.png" : "images/filter_a.png";
+    searchToggler = () => {
+        const {
+            searchFilterOpened,
+        } = this.state;
+
+        const {
+            // eslint-disable-next-line no-shadow
+            toggleSearchFilterShown,
+        } = this.props;
+        const selectedBackground = searchFilterOpened ? "images/filter_b.png" : "images/filter_a.png";
         let node;
         return (
             <div
                 id="filter_search_toggler"
-              ref={c => node = c}
-              onClick={() => {
-                    self.setState({ searchFilterOpened: !self.state.searchFilterOpened });
-                    this.props.toggleSearchFilterShown(node.getBoundingClientRect());
+                ref={(c) => { node = c; }}
+                onClick={() => {
+                    this.setState({ searchFilterOpened: !searchFilterOpened });
+                    toggleSearchFilterShown(node.getBoundingClientRect());
                 }}
                 style={{ backgroundImage: `url(${selectedBackground})` }}
+                role="button"
             />
         );
     }
 
     render() {
-        const self = this;
+        /* eslint-disable no-shadow */
+        const {
+            showNewScheduleModal,
+            showDuplicateScheduleModal,
+            showRenameScheduleModal,
+            showClearScheduleModal,
+            showDeleteScheduleModal,
+            scheduleNames,
+            scheduleSelected,
+            changeSchedule,
+        } = this.props;
+        /* eslint-enable no-shadow */
         return (
             <div id="searchbar" className="level">
                 <span className="level-left">
 
                     <div id="searchSelectContainer">
                         <Dropdown
-                            id="searchSelect" update_label def_active={0} def_text="Search By"
+                            id="searchSelect"
+                            update_label={true}
+                            def_active={0}
+                            def_text="Search By"
                             contents={[
                                 ["Course ID", () => {
                                 }],
                                 ["Keywords", () => {
                                 }],
                                 ["Instructor", () => {
-                                }],
+                                }]
                             ]}
                         />
                     </div>
 
                     <form
-                      className="ng-valid ng-dirty ng-valid-parse ng-submitted"
                         onSubmit={this.handleSubmit}
                     >
                         <input
-                            id="CSearch" type="text"
-                            className="input is-small is-rounded ng-valid ng-touched ng-not-empty ng-dirty ng-valid-parse"
+                            id="CSearch"
+                            type="text"
+                            className="input is-small is-rounded"
                             name="courseSearch"
                             autoComplete="off"
                             placeholder="Search for a department, course, or section"
-                            autoFocus="autofocus"
                         />
                     </form>
                     {this.searchToggler()}
                 </span>
 
                 <span className="level-right">
-
                     <div id="scheduleOptionsContainer">
                         <Dropdown
-                            id="scheduleDropdown" def_text="Schedule Options" contents={[
+                            id="scheduleDropdown"
+                            def_text="Schedule Options"
+                            contents={[
                                 ["New", () => {
-                                    self.props.showNewScheduleModal();
+                                    showNewScheduleModal();
                                 }],
                                 ["Download", () => {
                                 }],
                                 ["Duplicate", () => {
-                                    self.props.showDuplicateScheduleModal();
+                                    showDuplicateScheduleModal();
                                 }],
                                 ["Rename", () => {
-                                    self.props.showRenameScheduleModal();
+                                    showRenameScheduleModal();
                                 }],
                                 ["Clear", () => {
-                                    self.props.showClearScheduleModal();
+                                    showClearScheduleModal();
                                 }],
                                 ["Delete", () => {
-                                    self.props.showDeleteScheduleModal();
-                                }],
+                                    showDeleteScheduleModal();
+                                }]
                             ]}
                         />
                     </div>
-                    <button className="button"><span>Show Stars</span></button>
+                    <button className="button" type="button">
+                        <span>Show Stars</span>
+                    </button>
                     <a className="button" href="#UploadModal" id="ImportButton">
-					Import
-
+                    Import
                     </a>
-                    <button className="button"><span>Clear Search</span></button>
+                    <button className="button" type="button">
+                        <span>Clear Search</span>
+                    </button>
                     <div className="dropdown">
                         <div className="dropdown-trigger">
-                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" type="button">
                                 <span>
                                     <span className="selected_name">Schedules</span>
                                     <span className="icon is-small">
@@ -119,32 +147,14 @@ class SearchBar extends Component {
                                 </span>
                             </button>
                         </div>
-                        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                            <div
-                                className="dropdown-content ng-pristine ng-untouched ng-valid ng-not-empty"
-                                id="sched-select"
-                                role="menu"
-                            >
-                                <a
-                                    className="dropdown-item ng-binding ng-scope"
-                                >
-							Schedule
-                                </a>
-                                <a
-                                    className="dropdown-item ng-binding ng-scope"
-                                >
-						    Imported
-                                </a>
-                            </div>
-                        </div>
                     </div>
 
                     {/* Course summary dropdown */}
                     <SummaryDropdown />
                     <SchedulesDropdown
-                        scheduleNames={this.props.scheduleNames}
-                        scheduleSelected={this.props.scheduleSelected}
-                        changeSchedule={this.props.changeSchedule}
+                        scheduleNames={scheduleNames}
+                        scheduleSelected={scheduleSelected}
+                        changeSchedule={changeSchedule}
                     />
                 </span>
             </div>
@@ -171,3 +181,16 @@ const mapDispatchToProps = dispatch => ({
     startSearch: searchObj => dispatch(fetchCourseSearch(searchObj)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
+
+SearchBar.propTypes = {
+    startSearch: PropTypes.func.isRequired,
+    toggleSearchFilterShown: PropTypes.func.isRequired,
+    showNewScheduleModal: PropTypes.func.isRequired,
+    showDeleteScheduleModal: PropTypes.func.isRequired,
+    showRenameScheduleModal: PropTypes.func.isRequired,
+    showDuplicateScheduleModal: PropTypes.func.isRequired,
+    showClearScheduleModal: PropTypes.func.isRequired,
+    scheduleNames: PropTypes.arrayOf(PropTypes.string),
+    scheduleSelected: PropTypes.string,
+    changeSchedule: PropTypes.func.isRequired,
+};
--- frontend/src/components/search/search_result.js
+++ frontend/src/components/search/search_result.js
@@ -1,47 +1,47 @@
-import * as React from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export class SearchResult extends React.Component {
-    constructor(props) {
-        super(props);
-        this.course = props.course;
-    }
-
-    render() {
-        return (
-            <li
-                id={this.course.idDashed}
-                onClick={() => this.props.requestSectionInfo()}
+export function SearchResult({ course, requestSectionInfo }) {
+    return (
+        <li
+            id={course.idDashed}
+            onClick={() => requestSectionInfo()}
+            role="menuitem"
+        >
+            <span
+                className="PCR Qual"
+                style={{
+                    background: `rgba(45, 160, 240, ${course.pcrQShade})`,
+                    color: course.pcrQColor,
+                }}
             >
+                {course.revs.cQ || course.revs.cQT}
+            </span>
 
-                <span
-                    className="PCR Qual"
-                    style={{
-                        background: `rgba(45, 160, 240, ${this.course.pcrQShade})`,
-                        color: this.course.pcrQColor,
-                    }}
-                >
-                    {this.props.course.revs.cQ || this.props.course.revs.cQT}
-                </span>
+            <span
+                className="PCR Diff"
+                style={{
+                    background: `rgba(231, 76, 60, ${course.pcrDShade})`,
+                    color: course.pcrDColor,
+                }}
+            >
+                {course.revs.cD || course.revs.cDT}
+            </span>
 
-                <span
-                    className="PCR Diff"
-                    style={{
-                        background: `rgba(231, 76, 60, ${this.course.pcrDShade})`,
-                        color: this.course.pcrDColor,
-                    }}
-                >
-                    {this.props.course.revs.cD || this.props.course.revs.cDT}
-                </span>
+            <span className="cID">
+                {course.idSpaced}
+            </span>
 
-                <span className="cID">
-                    {this.props.course.idSpaced}
-                </span>
+            <span className="cTitle">
+                {course.courseTitle}
+            </span>
 
-                <span className="cTitle">
-                    {this.props.course.courseTitle}
-                </span>
-
-            </li>
-        );
-    }
+        </li>
+    );
 }
+
+SearchResult.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    course: PropTypes.object.isRequired,
+    requestSectionInfo: PropTypes.func.isRequired,
+};
